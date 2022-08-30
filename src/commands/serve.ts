296520aeed3c78a6cb65ba228002a50137b2da3b
JasonkayZK <271226192@qq.com>
2022-08-30 16:32:33 +0800

    feat: init serve

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -1,57 +1,67 @@
-import {Option} from "commander";
-// import {createServer} from "../libs";
+import {createServer} from "../libs";
 
-module.exports = function (options: Option[]) {
+interface ServeCmdOptions {
+
+    dir: string;
+
+    include?: string | undefined;
+
+    exclude?: string | undefined;
+
+    port: number;
+}
+
+module.exports = function (options: ServeCmdOptions) {
     console.log(options);
 
-    // createServer({
-    //     /*
-    //       The param req is a Restify Request entity
-    //       http://restify.com/docs/request-api/#request
-    //     */
-    //     getConfigHandler: req => {
-    //         /*
-    //           according to your api route
-    //           should return packageName, serviceName and method according to the request entity
-    //           eg. API Route Config /:package_name/:service_name/:method_name
-    //               Request Path /coderge.demo/ReadmeService/CopyText
-    //         */
-    //         return {
-    //             packageName: req.params['package_name'], // 'coderge.demo',
-    //             serviceName: req.params['service_name'], // 'ReadmeService',
-    //             methodName: req.params['method_name'] // 'CopyText'
-    //         };
-    //     },
-    //     /*
-    //       The param data is result of mock.js
-    //       https://github.com/nuysoft/Mock
-    //     */
-    //     responseHandler: (res, data) => {
-    //         /*
-    //           Can customize a response
-    //         */
-    //         // res.json(data);
-    //         // res.send(JSON.stringify(data))
-    //         res.json({ msg: 'ok', ret: 0, data });
-    //     },
-    //     /*
-    //       The param data is result of mock.js
-    //       https://github.com/nuysoft/Mock
-    //     */
-    //     /**
-    //      * Hack mock rules of template
-    //      * @param key protobuf message key
-    //      * @param type protobuf message key type (eg. string/int32/bool...)
-    //      * @param random
-    //      */
-    //     hackMockTpl: (key, type, random) => {
-    //         key = key.toLowerCase();
-    //         const keyTypeHas = (k: string, t: string) =>
-    //             type === t && key.indexOf(k) > -1;
-    //         if (keyTypeHas('icon', 'string')) return '@image';
-    //         else if (keyTypeHas('name', 'string')) return '@name';
-    //         return '';
-    //     },
-    // }).then(server => server.start());
+    createServer(options.dir, {
+        /*
+          The param req is a Restify Request entity
+          http://restify.com/docs/request-api/#request
+        */
+        getConfigHandler: req => {
+            /*
+              according to your api route
+              should return packageName, serviceName and method according to the request entity
+              eg. API Route Config /:package_name/:service_name/:method_name
+                  Request Path /coderge.demo/ReadmeService/CopyText
+            */
+            return {
+                packageName: req.params['package_name'], // 'coderge.demo',
+                serviceName: req.params['service_name'], // 'ReadmeService',
+                methodName: req.params['method_name'] // 'CopyText'
+            };
+        },
+        /*
+          The param data is result of mock.js
+          https://github.com/nuysoft/Mock
+        */
+        responseHandler: (res, data) => {
+            /*
+              Can customize a response
+            */
+            // res.json(data);
+            // res.send(JSON.stringify(data))
+            res.json({msg: 'ok', ret: 0, data});
+        },
+        /*
+          The param data is result of mock.js
+          https://github.com/nuysoft/Mock
+        */
+        /**
+         * Hack mock rules of template
+         * @param key protobuf message key
+         * @param type protobuf message key type (eg. string/int32/bool...)
+         * @param random
+         */
+        hackMockTpl: (key, type, random) => {
+            key = key.toLowerCase();
+            const keyTypeHas = (k: string, t: string) =>
+                type === t && key.indexOf(k) > -1;
+            if (keyTypeHas('icon', 'string')) return '@image';
+            else if (keyTypeHas('name', 'string')) return '@name';
+            return '';
+        },
+    }).then(server => server.start());
 
 }
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,6 +1,5 @@
 import mockjs, {MockjsRandom} from "mockjs";
 import restify, {Next, Request, Response} from "restify";
-import {initRepository} from "./git";
 import {getMethod, getMockTpl, loadProtobufDefinition} from "./mock";
 
 type ConfigHandler = (
@@ -10,7 +9,9 @@ type ConfigHandler = (
     serviceName: string;
     methodName: string;
 };
+
 type ResponseHandler = (res: restify.Response, data: any) => void;
+
 interface MockHandlerOptions {
     getConfigHandler: ConfigHandler;
     responseHandler?: ResponseHandler;
@@ -21,68 +22,67 @@ interface MockHandlerOptions {
     ) => string | (() => string);
 }
 
-// const generateMockHandler = (
-//     options: MockHandlerOptions,
-//     repository: string,
-// ) => {
-//     const {getConfigHandler, responseHandler, hackMockTpl} = options;
-//     const packageDefinition = initPackageDefinition(repository);
-//     return (req: Request, res: Response, next: Next) => {
-//         const {packageName, serviceName, methodName} = getConfigHandler(req);
-//         const method = getMethod(
-//             packageDefinition,
-//             packageName,
-//             serviceName,
-//             methodName,
-//         );
-//         const responseType = method?.responseType || "";
-//         const tpl = getMockTpl(
-//             packageDefinition,
-//             packageName,
-//             responseType,
-//             hackMockTpl,
-//         );
-//         const mockData = mockjs.mock(tpl);
-//         responseHandler ? responseHandler(res, mockData) : res.json(mockData);
-//         next();
-//     };
-// };
-//
-// export const createServer = async (options: MockHandlerOptions) => {
-//     const repository = await initRepository();
-//     const server = restify.createServer();
-//     // CORS
-//     server.use((req: Request, res: Response, next: Next) => {
-//         res.header("Access-Control-Allow-Credentials", true);
-//         res.header("Access-Control-Allow-Origin", req.headers.origin);
-//         next();
-//     });
-//     // HANDLER
-//     const handler = generateMockHandler(options, repository);
-//     // PAGE ROUTES
-//     server.get("/", (req: Request, res: Response, next: Next) => {
-//         res.end("<h1>Mock server in running !</h1>");
-//         next();
-//     });
-//     // API ROUTES
-//     server.opts("*", (req, res, next) => {
-//         res.header(
-//             "Access-Control-Allow-Methods",
-//             req.headers["access-control-request-methods"],
-//         );
-//         res.header(
-//             "Access-Control-Allow-Headers",
-//             req.headers["access-control-request-headers"],
-//         );
-//         res.end();
-//         next();
-//     });
-//     server.get("*", handler);
-//     server.post("*", handler);
-//     return {
-//         start: (port: number = 3333) =>
-//             server.listen(port, () =>
-//                 console.log("%s listening at %s", server.name, server.url),
-//             ),
-//     };
-// };
+const generateMockHandler = (
+    repository: string,
+    options: MockHandlerOptions,
+) => {
+    const {getConfigHandler, responseHandler, hackMockTpl} = options;
+    const packageDefinition = loadProtobufDefinition(repository);
+    return (req: Request, res: Response, next: Next) => {
+        const {packageName, serviceName, methodName} = getConfigHandler(req);
+        const method = getMethod(
+            packageDefinition,
+            packageName,
+            serviceName,
+            methodName,
+        );
+        const responseType = method?.responseType || "";
+        const tpl = getMockTpl(
+            packageDefinition,
+            packageName,
+            responseType,
+            hackMockTpl,
+        );
+        const mockData = mockjs.mock(tpl);
+        responseHandler ? responseHandler(res, mockData) : res.json(mockData);
+        next();
+    };
+};
+
+export const createServer = async (protobufRepoPath: string, options: MockHandlerOptions) => {
+    const server = restify.createServer();
+    // CORS
+    server.use((req: Request, res: Response, next: Next) => {
+        res.header("Access-Control-Allow-Credentials", true);
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        next();
+    });
+    // HANDLER
+    const handler = generateMockHandler(protobufRepoPath, options);
+    // PAGE ROUTES
+    server.get("/", (req: Request, res: Response, next: Next) => {
+        res.end("<h1>Mock server in running !</h1>");
+        next();
+    });
+    // API ROUTES
+    server.opts("*", (req, res, next) => {
+        res.header(
+            "Access-Control-Allow-Methods",
+            req.headers["access-control-request-methods"],
+        );
+        res.header(
+            "Access-Control-Allow-Headers",
+            req.headers["access-control-request-headers"],
+        );
+        res.end();
+        next();
+    });
+    server.get("*", handler);
+    server.post("*", handler);
+    return {
+        start: (port: number = 3333) =>
+            server.listen(port, () =>
+                console.log("%s listening at %s", server.name, server.url),
+            ),
+    };
+};
